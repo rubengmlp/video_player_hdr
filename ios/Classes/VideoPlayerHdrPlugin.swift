58d4ed0576db6c88a8bfe063d069ef7d33a4cbf0
rubengmlp <rubengmlp@gmail.com>
2025-05-29 08:43:56 +0200

    iPhone 11 uses tone mapping for HDR

--- ios/Classes/VideoPlayerHdrPlugin.swift
+++ ios/Classes/VideoPlayerHdrPlugin.swift
@@ -60,7 +60,6 @@ public class VideoPlayerHdrPlugin: NSObject, FlutterPlugin {
                 
                 // Devices with OLED screens support Dolby Vision
                 if deviceModel.contains("iPhone X") && !deviceModel.contains("iPhone XR") ||
-                   deviceModel.contains("iPhone 11") ||
                    deviceModel.contains("iPhone 11 Pro") ||
                    deviceModel.contains("iPhone 12") || 
                    deviceModel.contains("iPhone 13") || 
